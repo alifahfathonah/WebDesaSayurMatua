e21eedd05446c1740c94696110ea122d13454f70
andrahafiz <52515217+andrahafiz@users.noreply.github.com>
2020-07-23 22:01:48 +0700

    Update View_changepassword.php

--- Administrator/application/views/auth/View_changepassword.php
+++ Administrator/application/views/auth/View_changepassword.php
@@ -6,32 +6,28 @@
         <div class="login-box card">
             <div class="card-body">
                 <?= $this->session->flashdata('pesan'); ?>
-                <form class="form-horizontal form-material" id="loginform" action="<?= base_url('Auth/forget'); ?>" method="POST">
-                    <h2 class="box-title m-b-20 text-center">Reset Password</h2>
+                <form class="form-horizontal form-material" id="loginform" action="<?= base_url('Auth/changePassword'); ?>" method="POST">
+                    <h2 class=" box-title m-b-20 text-center">Reset Password</h2>
                     <h5 class="text-center font-weight-bold"><?= $this->session->userdata('reset'); ?></h5>
                     <p class="text-center"><?= $this->session->userdata('name'); ?></p>
                     <div class=" form-group">
                         <div class="col-xs-12">
-                            <input type="text" class="form-control" name="Email" placeholder="Email" autofocus>
+                            <input type="password" class="form-control" name="Password1" placeholder="Password" autofocus>
                         </div>
-                        <?= form_error('email', '<small class="text-danger">', '</small>'); ?>
+                        <?= form_error('Password1', '<small class="text-danger">', '</small>'); ?>
                     </div>
                     <div class="form-group">
                         <div class="col-xs-12">
-                            <input type="text" class="form-control" name="Email" placeholder="Email" autofocus>
+                            <input type="password" class="form-control" name="Password2" placeholder="Konfirmasi Password">
                         </div>
-                        <?= form_error('email', '<small class="text-danger">', '</small>'); ?>
+                        <?= form_error('Password2', '<small class="text-danger">', '</small>'); ?>
                     </div>
                     <div class="form-group text-center">
                         <div class="col-xs-12">
-                            <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Reset</button>
+                            <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">RESET PASSWORD</button>
                         </div>
                     </div>
-                    <div class="form-group m-b-0">
-                        <div class="col-sm-12 text-center">
-                            <a href="<?= base_url('Auth'); ?>" class="text-info m-l-5"><b>LOGIN</b></a>
-                        </div>
-                    </div>
+
             </div>
             </form>
 
