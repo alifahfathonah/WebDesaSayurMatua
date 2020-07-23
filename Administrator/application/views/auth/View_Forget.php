58f17b3a03ae438b0d8e952fae213e4d155aec06
andrahafiz <52515217+andrahafiz@users.noreply.github.com>
2020-07-23 21:31:15 +0700

    Update View_Forget.php

--- Administrator/application/views/auth/View_Forget.php
+++ Administrator/application/views/auth/View_Forget.php
@@ -5,13 +5,19 @@
     <div class="login-register" style="background-image:url(../assets/images/background/login-register.jpg);">
         <div class="login-box card">
             <div class="card-body">
-                <form class="form-horizontal form-material" id="loginform" action="index.html">
-                    <h3 class="box-title m-b-20">Recover Password</h3>
+                <?= $this->session->flashdata('pesan'); ?>
+                <form class="form-horizontal form-material" id="loginform" action="<?= base_url('Auth/forget'); ?>" method="POST">
+                    <h3 class="box-title m-b-20">Reset Password</h3>
+                    <div class="form-group">
+                        <p class="text-muted">
+                            <small> Kami akan mengirimkan email konfirmasi pergantian password hanya ke email yang telah terdaftar disistem kami</small>
+                        </p>
+                    </div>
                     <div class="form-group">
                         <div class="col-xs-12">
                             <input type="text" class="form-control" name="Email" placeholder="Email" autofocus>
                         </div>
-                        <?= form_error('email', '<label id="description-error" class="error" for="description">', '</label>'); ?>
+                        <?= form_error('email', '<small class="text-danger">', '</small>'); ?>
                     </div>
                     <div class="form-group text-center">
                         <div class="col-xs-12">
