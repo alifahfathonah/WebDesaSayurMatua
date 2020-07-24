00cd0013e4f666d1c1e07b01d3b380bb62c9b66e
andrahafiz <52515217+andrahafiz@users.noreply.github.com>
2020-07-24 13:43:05 +0700

    update blocked

--- Administrator/application/views/auth/blocked.php
+++ Administrator/application/views/auth/blocked.php
@@ -1,44 +1,57 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <title>500 | Bootstrap Based Admin Template - Material Design</title>
-    <!-- Favicon-->
-    <link rel="icon" href="<?= base_url(); ?>assets/favicon.ico" type="image/x-icon">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url() ?>assets/images/favicon.png">
+    <title>Desa Sayur Matua</title>
 
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+    <!-- Custom CSS -->
+    <link href="<?= base_url() ?>assets/dist/css/style.min.css" rel="stylesheet">
+    <!-- page css -->
+    <link href="<?= base_url() ?>assets/dist/css/pages/error-pages.css" rel="stylesheet">
 
-    <!-- Bootstrap Core Css -->
-    <link href="<?= base_url(); ?>assets/plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
-
-    <!-- Waves Effect Css -->
-    <link href="<?= base_url(); ?>assets/plugins/node-waves/waves.css" rel="stylesheet" />
-
-    <!-- Custom Css -->
-    <link href="<?= base_url(); ?>assets/css/style.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
 </head>
 
-<body class="five-zero-zero">
-    <div class="five-zero-zero-container">
-        <div class="error-code">403</div>
-        <div class="error-message">Access Forbidden</div>
-        <div class="button-place">
-            <a href="<?= base_url(); ?>" class="btn btn-default btn-lg waves-effect">GO TO HOMEPAGE</a>
+<body class="skin-default fixed-layout">
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <section id="wrapper" class="error-page">
+        <div class="error-box">
+            <div class="error-body text-center">
+                <h1>404</h1>
+                <h3 class="text-uppercase">Halaman Tidak di Temukan!</h3>
+                <p class="text-muted m-t-30 m-b-30">DESA SAYUR MATUA, KABUPATEN PADANG LAWAS, SUMATERA UTARA</p>
+                <a href="<?= base_url(); ?>" class="btn btn-info btn-rounded waves-effect waves-light m-b-40">Kembali</a>
+            </div>
+
         </div>
-    </div>
-
-    <!-- Jquery Core Js -->
-    <script src="<?= base_url(); ?>assets/plugins/jquery/jquery.min.js"></script>
-
-    <!-- Bootstrap Core Js -->
-    <script src="<?= base_url(); ?>assets/plugins/bootstrap/js/bootstrap.js"></script>
-
-    <!-- Waves Effect Plugin Js -->
-    <script src="<?= base_url(); ?>assets/plugins/node-waves/waves.js"></script>
+    </section>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="<?= base_url() ?>assets/assets/node_modules/jquery/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="<?= base_url() ?>assets/assets/node_modules/popper/popper.min.js"></script>
+    <script src="<?= base_url() ?>assets/assets/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!--Wave Effects -->
+    <script src="<?= base_url() ?>assets/dist/js/waves.js"></script>
 </body>
 
 </html>
